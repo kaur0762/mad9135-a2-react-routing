d7727cd82139784b76ae2380fad866afb61d0111
kaur0762 <kaur0762@algonquinlive.com>
2022-10-09 22:23:43 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <header className="App-header">
+        <h1>
+          React Route
+        </h1>
+      </header>
+  )
+}
+
+export default Header;
