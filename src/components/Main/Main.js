726075cefacff0a803df05c561d9035b9dd97999
kaur0762 <kaur0762@algonquinlive.com>
2022-10-12 12:07:40 -0400

    basic structure

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Output from "../Output/Output";
+
+function Main(){
+  return(
+    <>
+    <div>
+      <input type="text"/>
+      <button>Search</button>
+    </div>
+    <Output />
+    </>
+  )
+}
+
+export default Main;
